be387344f28c702fc458a74e60caeb6a93dc5086
FeliΞ <moongod101@hotmail.com>
2021-12-13 18:51:38 +0000

    query the DB inside the HKID auth guard

--- backend/src/auth/header-hkid.guard.ts
+++ backend/src/auth/header-hkid.guard.ts
@@ -1,19 +1,36 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { isNil } from "lodash";
-import { Observable } from "rxjs";
+import { PrismaServiceV2 } from "src/prisma/prisma.service";
 
 @Injectable()
 export class HeaderHkidGuard implements CanActivate {
-  canActivate(
+  
+  constructor(
+    private readonly prisma: PrismaServiceV2,
+  ) {}
+  
+  async canActivate(
     context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const HKIDHash: string = req.headers?.["x-hkidhash"];
+    
+    const userRecord = await this.prisma.user.findFirst({
+      where: {
+        HKIDHash,
+      }
+    });
 
     // the incoming request didn't provied a header "X-HKIDHash"
     if (isNil(HKIDHash)) {
       return false;
     }
+    
+    if(isNil(userRecord)) {
+      return false;
+    }
+
     return true;
+
   }
 }
